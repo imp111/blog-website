81907da73c49409f528f15733fb24dd987e132e4
imp111 <alexsandartenev@gmail.com>
2023-01-31 14:38:21 +0200

    migration successful

--- Blog-Website/Models/Post.cs
+++ Blog-Website/Models/Post.cs
@@ -1,9 +1,21 @@
-﻿namespace Blog_Website.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog_Website.Models
 {
     public class Post
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Title: ")]
         public string Title { get; set; } = "";
+
+        [DisplayName("Body: ")]
+        [Required]
         public string Body { get; set; } = "";
+
         public DateTime Created { get; set; } = DateTime.Now;
     }
 }
