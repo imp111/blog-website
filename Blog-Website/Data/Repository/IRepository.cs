7ffdfa0ffff5e8e6b341736df4c30c5f78f87c3e
imp111 <alexsandartenev@gmail.com>
2023-02-06 19:15:59 +0200

    IRepository and Repository added

---
+++ Blog-Website/Data/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blog_Website.Models;
+
+namespace Blog_Website.Data.Repository
+{
+    public interface IRepository
+    {
+        Post GetPost(int id);
+        List<Post> GetAllPosts(int id);
+        bool AddPost(Post post);
+        bool UpdatePost(Post post);
+        bool RemovePost(int id);
+    }
+}
---
+++ Blog-Website/Data/Repository/Repository.cs
@@ -0,0 +1,32 @@
+﻿using Blog_Website.Models;
+
+namespace Blog_Website.Data.Repository
+{
+    public class Repository : IRepository
+    {
+        public bool AddPost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetAllPosts(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemovePost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
