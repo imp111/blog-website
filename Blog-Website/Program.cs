e10f43668b778184c6ec7cb1c2081a19e47d695a
imp111 <alexsandartenev@gmail.com>
2023-02-07 20:33:11 +0200

    changes to program.cs

--- Blog-Website/Program.cs
+++ Blog-Website/Program.cs
@@ -39,4 +39,30 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+var scope = app.Services.CreateScope();
+var ctx = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); // database context
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>(); // manages all the user accounts
+var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>(); // manages all the roles that you can assign an account to
+
+ctx.Database.EnsureCreated();
+
+var adminRole = new IdentityRole("Admin");
+if (!ctx.Roles.Any())
+{
+    // create a role
+    roleManager.CreateAsync(adminRole);
+}
+
+if (!ctx.Users.Any(u => u.UserName == "Admin"))
+{
+    // create admin user
+    var adminUser = new IdentityUser { UserName = "admin", Email = "admin@test.com" };
+
+    string password = "praseta123";
+    userManager.CreateAsync(adminUser, password);
+
+    // add role to user
+    //userManager
+}
+
 app.Run();
