2faa9bd8dc4c1b0dfc24e01b4cecbe537e764bae
imp111 <alexsandartenev@gmail.com>
2023-02-08 23:55:41 +0200

    admin role added to project

--- Blog-Website/Program.cs
+++ Blog-Website/Program.cs
@@ -11,7 +11,14 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnention"))); // Database added
 
-builder.Services.AddIdentityCore<IdentityUser>()
+builder.Services.AddIdentityCore<IdentityUser>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 10;
+}
+)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
@@ -39,30 +46,39 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-var scope = app.Services.CreateScope();
-var ctx = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); // database context
-var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>(); // manages all the user accounts
-var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>(); // manages all the roles that you can assign an account to
-
-ctx.Database.EnsureCreated();
-
-var adminRole = new IdentityRole("Admin");
-if (!ctx.Roles.Any())
+try
 {
-    // create a role
-    roleManager.CreateAsync(adminRole);
+    var scope = app.Services.CreateScope();
+    var ctx = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); // database context
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>(); // manages all the user accounts
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>(); // manages all the roles that you can assign an account to
+
+    ctx.Database.EnsureCreated();
+
+    var adminRole = new IdentityRole("Admin");
+    if (!ctx.Roles.Any())
+    {
+        // create a role
+        roleManager.CreateAsync(adminRole);
+    }
+
+    if (!ctx.Users.Any(u => u.UserName == "Admin"))
+    {
+        // create admin user
+        var adminUser = new IdentityUser { UserName = "admin", Email = "admin@test.com" };
+
+        string password = "praseta123";
+        userManager.CreateAsync(adminUser, password);
+
+        // add role to user
+        userManager.AddToRoleAsync(adminUser, adminRole.Name);
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
 }
 
-if (!ctx.Users.Any(u => u.UserName == "Admin"))
-{
-    // create admin user
-    var adminUser = new IdentityUser { UserName = "admin", Email = "admin@test.com" };
-
-    string password = "praseta123";
-    userManager.CreateAsync(adminUser, password);
-
-    // add role to user
-    //userManager
-}
 
 app.Run();
