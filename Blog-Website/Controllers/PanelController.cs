06dea8b4b909a5bc36fc8ea445df7f8295f87ac8
imp111 <alexsandartenev@gmail.com>
2023-02-09 03:18:11 +0200

    admin panel controller and view added

---
+++ Blog-Website/Controllers/PanelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Website.Controllers
+{
+    public class PanelController : Controller
+    {
+        public PanelController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
