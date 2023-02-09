b98c9b4f6b4ab0317b2d00e2d186c57d50a4e940
imp111 <alexsandartenev@gmail.com>
2023-02-09 03:35:47 +0200

    authorize admin panel

--- Blog-Website/Controllers/PanelController.cs
+++ Blog-Website/Controllers/PanelController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blog_Website.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Admin")]
     public class PanelController : Controller
     {
         public PanelController()
